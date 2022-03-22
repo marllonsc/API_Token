8c80aab87b57f13c7bb3d587eba16c64ee82a92a
Marllon Costa <marllonsc@gmail.com>
2022-03-22 11:12:13 -0300

    first commit on project

---
+++ src/main/java/com/msc/telecon/TeleconApplication.java
@@ -0,0 +1,13 @@
+package com.msc.telecon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeleconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeleconApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msc/telecon/TeleconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msc.telecon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeleconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
